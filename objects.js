cef0a156711ecdfe733ee6ec621d0bfebacdab73
Louis Shi <louis.shi@uconn.edu>
2018-03-10 20:36:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = {Object: 'playlist'};
 function updatePlaylist (playlist, artistName, songTitle) {
   var playlist;
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
-  return playlist
+  return playlist;
 }
